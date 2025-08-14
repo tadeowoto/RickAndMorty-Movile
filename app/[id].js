003bf8b4d13a3d6e47009157b5161ecdbb69a120
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-08-14 19:21:50 -0300

    feat: add search character by id

--- app/[id].js
+++ app/[id].js
@@ -2,14 +2,31 @@ import { Text, View, Pressable } from "react-native";
 import { Link } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
 import { Stack } from "expo-router";
+import { fetchCharacterById } from "../lib/characters";
+import { useEffect, useState } from "react";
 
-export default function Detail() {
+export default async function Detail() {
   const { id } = useLocalSearchParams();
+
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    const loadCharacter = async () => {
+      try {
+        const fetchedCharacter = await fetchCharacterById({ id });
+        setCharacter(fetchedCharacter);
+      } catch (error) {
+        console.error("Error fetching character:", error);
+      }
+    };
+    loadCharacter();
+  }, []);
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <Stack.Screen
         options={{
-          headerTitle: `Character ${id}`,
+          headerTitle: `${character?.name || "Character Detail"}`,
           headerStyle: { backgroundColor: "#1e293b" },
           headerTintColor: "#fff",
         }}
